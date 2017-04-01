ca73fb72662b258a50d4be216044ebbeb9dad2b0
Rodrigo Leme <lemerodrigo@gmail.com>
2017-04-01 11:39:42 -0700

    Fixing config paths

--- routes/audioClips.js
+++ routes/audioClips.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const userTokenValidator = require('./../middlewares/userTokenValidator');
-const upload = multer({ dest: 'uploads/tmp/' });
+const upload = multer({ dest: 'audio-descriptions-files/tmp/' });
 const router = express.Router();
 const audioClipsController = require('../controllers/audioClipsController');
 
--- shared/config.js
+++ shared/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = () => {
   const apiVersion = 'v1';
-  const uploadsRootDir = path.join(__dirname, '..', 'uploads', 'current');
+  const uploadsRootDir = path.join(__dirname, '..', 'audio-descriptions-files', 'current');
   const googleClientId = '1056671841574-e1r4soednlur8hl2sl0ooumpvftt1s2k.apps.googleusercontent.com';
   const cryptoSecret = '8c628449c5102aeabd49b5dc3a2a516ea6';
   const cryptoSeed = '#@2$d32467ERvdd';
