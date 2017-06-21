43b0eba33b7496a81e800c20bd8eef1ef60d3d53
Rodrigo Leme <lemerodrigo@gmail.com>
2017-06-20 22:49:36 -0300

    New ios key

--- shared/config.js
+++ shared/config.js
@@ -10,7 +10,7 @@ module.exports = () => {
     uploadsRootDirToDelete = '/mnt/ebs/audio-descriptions-files';
   }
   const googleClientId = '1056671841574-e1r4soednlur8hl2sl0ooumpvftt1s2k.apps.googleusercontent.com';
-  const googleiOSClientId = '1056671841574-178q3to30qhqaerj1ej06cj7crfcdtb2.apps.googleusercontent.com';
+  const googleiOSClientId = '1056671841574-5qo81vnn336mlds8mjk33cpkron1pv80.apps.googleusercontent.com';
   const cryptoSecret = '8c628449c5102aeabd49b5dc3a2a516ea6';
   const cryptoSeed = '#@2$d32467ERvdd';
   return {
