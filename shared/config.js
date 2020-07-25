453ad8ddd2d398806153adc317991286b9f96564
jcastan6 <josecastanon4@gmail.com>
2020-07-25 12:20:55 -0700

    logging current api key

--- shared/config.js
+++ shared/config.js
@@ -1,23 +1,17 @@
 const fetch = require("node-fetch");
 const path = require("path");
+var moment = require("moment");
 // const NODE_ENV = process.env.NODE_ENV;
 const NODE_ENV = "prd";
+
 let currentApiKeyIndex = 0;
 
-/// IOS KEYS
-// const youTubeApiKeys = [
-//
-//
-// ];
-
-// DEV SITE KEYS
-const youTubeApiKeys = ["AIzaSyBQFD0fJoEO2l8g0OIrqbtjj2qXXVNO__U"];
-
-// Live SITE KEYS
-// const youTubeApiKeys = [
-//   "AIzaSyDV8QMir3NE8S2jA1GyXvLXyTuSq72FPyE",
-//   "AIzaSyAfU2tpVpMKmIyTlRljnKfPUFWXrNXg21Q"
-// ];
+//Live SITE KEYS
+const youTubeApiKeys = [
+  "AIzaSyDV8QMir3NE8S2jA1GyXvLXyTuSq72FPyE",
+  "AIzaSyAfU2tpVpMKmIyTlRljnKfPUFWXrNXg21Q",
+  "AIzaSyBWQ2o3N0MVc8oP96JvWVVwqjxpEOgkhQU"
+];
 
 // reset to first key at midnight, when all keys are reset.
 var midnight = "0:00:00";
@@ -95,11 +89,10 @@ module.exports = () => {
   const listenByCodeAppKey = "yAOzHAy9LQBJQGtshcIGJX368IbC4Enx";
   const youTubeApiUrl = "https://www.googleapis.com/youtube/v3";
   // const youTubeApiKey = "AIzaSyCEMAn_7h1wgIgZ4xhLbQUDuLKlkmvgLHs";     // !!! occupied by ios app !!! (google cloud project: youdescribesfsu@gmail.com -> youdescribe)
-  // const youTubeApiKey = "AIzaSyDV8QMir3NE8S2jA1GyXvLXyTuSq72FPyE";     // !!! occupied by https://youdescribe.org !!! (google cloud project: youdescribeadm@gmail.com -> youdescribe-0126)
-  // const currentApiKey = "AIzaSyBQFD0fJoEO2l8g0OIrqbtjj2qXXVNO__U"; // !!! occupied by https://dev.youdescribe.org !!! (google cloud project: youdescribeadm@gmail.com -> youdescribe-0127)
-  // const youTubeApiKey = "AIzaSyBWQ2o3N0MVc8oP96JvWVVwqjxpEOgkhQU";     // !!! occupied by http://18.221.192.73:3001 !!! (google cloud project: youdescribeadm@gmail.com -> youdescribe-0612)
-  // const youTubeApiKey = "AIzaSyAfU2tpVpMKmIyTlRljnKfPUFWXrNXg21Q";     // free to use (google cloud project: youdescribeadm@gmail.com -> youdescribe-0613)
-
+  //const youTubeApiKey = "AIzaSyDV8QMir3NE8S2jA1GyXvLXyTuSq72FPyE";        // !!! occupied by https://youdescribe.org !!! (google cloud project: youdescribeadm@gmail.com -> youdescribe-0126)
+  // const youTubeApiKey = "AIzaSyBQFD0fJoEO2l8g0OIrqbtjj2qXXVNO__U";     // !!! occupied by https://dev.youdescribe.org !!! (google cloud project: youdescribeadm@gmail.com -> youdescribe-0127)
+  //const youTubeApiKey = "AIzaSyBWQ2o3N0MVc8oP96JvWVVwqjxpEOgkhQU";     // !!! occupied by http://18.221.192.73:3001 !!! (google cloud project: youdescribeadm@gmail.com -> youdescribe-0612)
+  //const youTubeApiKey = "AIzaSyAfU2tpVpMKmIyTlRljnKfPUFWXrNXg21Q";     // free to use (google cloud project: youdescribeadm@gmail.com -> youdescribe-0613)
   const googleCloudStorageKeyFilename =
     "shared/youdescribe-stats-846041efde0c.json";
   const googleCloudStorageProjectId = "youdescribe-stat-1569864136126";
