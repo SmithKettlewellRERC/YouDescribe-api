680ce3311c80d3fc47b2683bcbd9b4d2eab432fc
jcastan6 <josecastanon4@gmail.com>
2020-11-19 16:27:57 -0800

    email system updated

--- controllers/videosController.js
+++ controllers/videosController.js
@@ -750,11 +750,6 @@ const videosController = {
         }
       );
     }
-  },
-
-  getCurrentKey: (req, res) => {
-    print(res);
-    res.send(conf.youTubeApiKey);
   }
 };
 
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const usersController = require('../controllers/usersController');
+const usersController = require("../controllers/usersController");
 
+router.post("/sendemail", usersController.sendMail);
 router.post("/sendoptinemail", usersController.sendOptInEmail);
-router.post("/sendvideoremappingemail", usersController.sendVideoRemappingEmail);
+router.post(
+  "/sendvideoremappingemail",
+  usersController.sendVideoRemappingEmail
+);
 router.post("/sendvideoindexeremail", usersController.sendVideoIndexerEmail);
 router.post("/updateoptin", usersController.updateOptIn);
-router.get('/:userId', usersController.getOne);
+router.get("/:userId", usersController.getOne);
 
 module.exports = router;
