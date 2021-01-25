99a964d07078ea9e300839f69de810516b136b6e
jcastan6 <josecastanon4@gmail.com>
2021-01-25 14:45:52 -0800

    fixed stats function

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -28,7 +28,7 @@ const cluster = require("cluster");
 
 //weekly summary email
 
-cron.schedule("45 14 * * 1", async () => {
+cron.schedule("47 14 * * 1", async () => {
   if (cluster.isMaster) {
     weeklyVideos = await weeklyStatistics.weeklyVideoCount();
     weeklyUsers = await weeklyStatistics.weeklyUserCount();
