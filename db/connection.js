01432f2c48a74f6e34e28a640b1fa855af97f710
Benjamin Kao <benjamin.kao00@gmail.com>
2022-09-15 12:40:31 -0700

    Setup CI/CD Pipeline
    - Created Dockerfile to build image with correct env variables
    - Updated package.json with correct npx scripts
    - Updated shared/config.js to grab the correct APIKey depending on
      the environment
    - Created .gitlab-ci.yml based off of YouDescribe-app version
    - Updated db/connection.js to use an environment variable to decide
      what the DB_HOST should be

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
-const config = require("./config");
+// const config = require("./config");
+
+var DB_HOST = "localhost";
+
+if(process.env.DB_HOST != null && process.env.DB_HOST.length > 0) {
+  DB_HOST = process.env.DB_HOST;
+}
 
 //deployment
 //mongodb://${config.username}:${config.password}@${config.hostname}/${config.database}
 //local
 //mongodb://localhost:27017/youdescribe
 const db = mongoose.connect(
-  `mongodb://youdescribe:E2E32fgklsdfiefwefmm6gyT@localhost:27020/youdescribe`,
+  `mongodb://youdescribe:E2E32fgklsdfiefwefmm6gyT@${DB_HOST}:27020/youdescribe`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
