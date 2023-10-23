093f0c8c274a1b2e146301451225fae135ae2dce
Youdescribe SFSU <54917817+youdescribe-sfsu@users.noreply.github.com>
2023-10-23 11:51:55 -0700

    Dev (#45)
    * Added log for req
    
    * Log for req.user
    
    * Added code to decypt data
    
    * added decrypt data
    
    * console for req headers
    
    * updated user creds to authorization
    
    * Added voted property in wishlist api
    
    * fixed wishlist error
    
    * added logs for wishlist
    
    * fixed wishlist
    
    * Added code to remove from wishlist
    
    * Updated getAll for Mobile App
    
    * Bug fixes
    
    * Update port number for YouDescribe-API deployment in staging environment
    
    * Update docker commands to stop and remove containers with specific names
    
    * updated gitlab ci
    
    * Update docker deployment scripts and MongoDB connection port for staging and prod environments
    
    * Update docker deployment scripts and MongoDB connection port for staging and prod environments
    
    ---------
    
    Co-authored-by: SFState <vishalsharma1907@gmail.com>

--- db/connection.js
+++ db/connection.js
@@ -7,12 +7,18 @@ if(process.env.DB_HOST != null && process.env.DB_HOST.length > 0) {
   DB_HOST = process.env.DB_HOST;
 }
 
+var DB_PORT = "27020";
+
+if(process.env.DB_PORT != null && process.env.DB_PORT.length > 0) {
+  DB_PORT = process.env.DB_PORT;
+}
+
 //deployment
 //mongodb://${config.username}:${config.password}@${config.hostname}/${config.database}
 //local
 //mongodb://localhost:27017/youdescribe
 const db = mongoose.connect(
-  `mongodb://youdescribe:E2E32fgklsdfiefwefmm6gyT@${DB_HOST}:27020/youdescribe`,
+  `mongodb://youdescribe:E2E32fgklsdfiefwefmm6gyT@${DB_HOST}:${DB_PORT}/youdescribe`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
