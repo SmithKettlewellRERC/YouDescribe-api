cacdc502a84daf6e4ce4dd3c4a9bf722617e59fc
XuanDuyAnhNguyen <duyanh1202@gmail.com>
2025-02-24 14:09:21 -0800

    fix bug apple signin API

--- models/user.js
+++ models/user.js
@@ -119,7 +119,7 @@ passport.use(
       const firstTimeUser = typeof req.query['user'] === 'string' ? JSON.parse(req.query['user']) : undefined;
       const newToken = crypto
         .createHmac('sha256', conf.cryptoSecret)
-        .update(conf.cryptoSeed + moment().utc().format('YYYYMMDDHHmmss'))
+        .update(conf.cryptoSeed + nowUtc())
         .digest('hex');
 
       try {
